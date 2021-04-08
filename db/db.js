48ef3590cee6213fee4f7fb006df385d32d15aa9
jucamo0713 <jucamo.0713@gmail.com>
2021-04-08 09:57:55 -0500

    Backend

---
+++ db/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  multipleStatements: true
+});
+
+pool.on('remove', client => {
+  console.log("Disconnect");
+  //console.log(client)
+  //console.log("Disconnect")
+});
+console.log("Succesfull conection with db "+ pool.options.database);
+module.exports = pool;
