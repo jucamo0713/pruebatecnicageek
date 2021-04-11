cb7236478309d158462d3df9041d65e3f641678c
jucamo0713 <jucamo.0713@gmail.com>
2021-04-10 20:11:27 -0500

    minucia

--- controllers/compras.js
+++ controllers/compras.js
@@ -13,7 +13,7 @@ const controller ={
             const conection= await pg.connect();
             conection.query("SELECT * FROM compras WHERE usuario=$1",[id])
             .then((data)=>{
-             res.json({status:true});
+             res.json({status:true, ...data});
             })
             .catch((err)=>{
                 res.json({status:false, err});
